fd6643be26e192847a5b8262711c32df229621ac
Diego Carrillo Cuéllar <diegocarrillocuellar.2005@gmail.com>
2024-02-13 13:05:24 +0100

    segundo commit

--- src/newpackage/HolaMundo.java
+++ src/newpackage/HolaMundo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package newpackage;
 
 /**
@@ -10,6 +7,6 @@ package newpackage;
  */
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
     }
 }
